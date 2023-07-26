8489e128601ecc453b9c6091bac4c87001623916
user <stratecide@gmail.com>
2023-07-26 15:55:18 +0200

    modpack didn't like ModifyConstant

--- src/main/java/com/stratecide/potion_config/mixin/PlayerEntityMixin.java
+++ src/main/java/com/stratecide/potion_config/mixin/PlayerEntityMixin.java
@@ -1,13 +1,19 @@
 package com.stratecide.potion_config.mixin;
 
 import com.stratecide.potion_config.effects.CustomStatusEffect;
+import net.minecraft.block.BlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tag.FluidTags;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -15,12 +21,21 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         super(entityType, world);
     }
 
-    @ModifyConstant(method = "getBlockBreakingSpeed", constant = @Constant(floatValue = 5.0f))
-    private float applyFinesse(float constant) {
+    @Inject(method = "getBlockBreakingSpeed", at = @At("TAIL"), cancellable = true)
+    private void applyFinesse(BlockState block, CallbackInfoReturnable<Float> cir) {
         if (this.hasStatusEffect(CustomStatusEffect.FINESSE)) {
-            // 5 -> 4 -> 3.4 -> 3 -> 2.7 -> 2.5 -> 2.333
-            return 1.0f + (constant - 1.0f) * 3.0f / (4.0f + (float) this.getStatusEffect(CustomStatusEffect.FINESSE).getAmplifier());
+            int count = 0;
+            if (this.isSubmergedIn(FluidTags.WATER) && !EnchantmentHelper.hasAquaAffinity(this))
+                count += 1;
+            if (!this.onGround)
+                count += 1;
+            double result = cir.getReturnValue();
+            for (int i = 0; i < count; i++) {
+                result *= 5.0;
+                // 5 -> 4 -> 3.4 -> 3 -> 2.7 -> 2.5 -> 2.333
+                result /= 1.0 + 12.0 / (4.0 + (double) this.getStatusEffect(CustomStatusEffect.FINESSE).getAmplifier());
+            }
+            cir.setReturnValue((float) result);
         }
-        return constant;
     }
 }
